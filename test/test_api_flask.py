92e87cececfe82bb322e6de15363b0fbe8f550fc
david <david.rigaudie@itarverne.com>
2022-01-25 11:04:05 +0100

    [FEATURE] Add route maker

--- test/test_api_flask.py
+++ test/test_api_flask.py
@@ -1,12 +1,13 @@
 import pytest
 
+@pytest.mark.route
 def test_homepage(create_app):
     with create_app.test_client() as test_client:
         response = test_client.get('/')
         assert response.status_code == 200
         assert b"Hello" in response.data
 
-
+@pytest.mark.route
 @pytest.mark.parametrize("param", [1, "2", -3])
 def test_param_other(create_app, param):
     with create_app.test_client() as test_client:
