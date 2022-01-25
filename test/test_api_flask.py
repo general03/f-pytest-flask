ef409e95c6582017d7cdb0e9c9f854665610cfaa
david <david.rigaudie@itarverne.com>
2022-01-25 10:45:28 +0100

    [FEATURE] Add url check with parametrize

--- test/test_api_flask.py
+++ test/test_api_flask.py
@@ -1,6 +1,15 @@
+import pytest
 
 def test_homepage(create_app):
     with create_app.test_client() as test_client:
         response = test_client.get('/')
         assert response.status_code == 200
         assert b"Hello" in response.data
+
+
+@pytest.mark.parametrize("param", [1, "2", -3])
+def test_param_other(create_app, param):
+    with create_app.test_client() as test_client:
+        response = test_client.get(f"/other?page={param}")
+        assert response.status_code == 200
+        assert bytes(f"Page : {param}", "utf-8") in response.data
